9fe6b2cf48474ef061a0a6b50d12fee82df7d49e
teofurlan <105223297+teofurlan@users.noreply.github.com>
2024-03-21 14:05:49 -0300

    Added the functionality to create more tasks

--- src/components/Dropdown/Dropdown.js
+++ src/components/Dropdown/Dropdown.js
@@ -8,8 +8,38 @@ dropdownCheckbox.addEventListener("change", () => {
   }
 });
 
-document.getElementById('newTask').addEventListener('keypress', (event) => {
-  if (event.key === 'Enter') {
-    console.log('added new task!')
+// Event Listener that triggers when the the Dropdown's text input is focused and the enter is pressed
+document.getElementById("newTask").addEventListener("keypress", (event) => {
+  if (event.key === "Enter") {
+    event.preventDefault();
+    // Adds the new task component to the taskContainer inside the Card
+    document
+      .getElementById("taskContainer")
+      .appendChild(createNewTask(document.getElementById("newTask").value));
+    // Resets the text input
+    document.getElementById("newTask").value = "";
   }
-})
+});
+
+// Takes taskDescription (string) as parameter and returns the node that represents the Task.astro component with the taskDescription as content of its label 
+const createNewTask = (taskDescription) => {
+  // Selects the template that's used to create a new Task component and clones it
+  const taskTemplate = document
+    .getElementById("taskContainer")
+    .querySelector(".template")
+    .cloneNode(true);
+  // Makes it visible
+  taskTemplate.style.display = "flex";
+  // Removes the selectorClass that makes the filtering buttons ignore this template task
+  taskTemplate.classList.remove('template')
+  // Add 'task' as its selectorClass so it is filtered by the buttons
+  taskTemplate.classList.add('task')
+  // Set new ids
+  const newId = Math.random().toString(36).substring(7);
+  taskTemplate.querySelector("input").id = `checkbox-${newId}`;
+  taskTemplate.querySelector("label").id = `label-${newId}`;
+  taskTemplate.querySelector(".strikethrough").id = `strikethrough-${newId}`;
+  // Set new task description
+  taskTemplate.querySelector("label").innerHTML = taskDescription;
+  return taskTemplate;
+};
