d843db791a63a8df5c925c3c6fed51155340595c
teofurlan <105223297+teofurlan@users.noreply.github.com>
2024-03-24 21:26:28 -0300

    Removed the Menu folder and changed the name of the Menu2 one to Menu

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,12 +1,37 @@
+let menuButton = document.getElementById("menuButton");
+let menu = document.getElementById("Menu");
 
-let menuDisplayButton = document.getElementById("menuDisplayButton")
-let optionsContainer = document.getElementById("optionsContainer")
-menuDisplayButton?.addEventListener("click", () => {
-  if (optionsContainer) {
-    if (optionsContainer.classList.contains("menu-options__opened")) {
-      optionsContainer.classList.remove("menu-options__opened")
-    } else {
-      optionsContainer.classList.add("menu-options__opened")
-    }
-  }
-})
+let mainMenu = document.getElementById("mainMenu");
+let timeButton = document.getElementById("timeButton");
+
+let timeMenuButtons = document.querySelectorAll(".time-menu__button");
+
+let selectedTime
+
+timeMenu.classList.add("time-menu--closed");
+
+menuButton.addEventListener("click", () => {
+  menu.classList.toggle("menu--opened");
+});
+
+timeButton.addEventListener("click", () => {
+  mainMenu.classList.toggle("main-menu--closed");
+  timeMenu.classList.toggle("time-menu--closed");
+});
+
+// timeMenu.addEventListener('click', (event) => {
+//     if (event.target.classList.contains("time-menu__button") || event.target.classList.contains("menu__text")) {
+//         mainMenu.classList.toggle("main-menu--closed");
+//         timeMenu.classList.toggle("time-menu--closed")
+//     }
+// })
+
+timeMenuButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    console.log(button.querySelector(".menu__subtext").textContent.substring(0, 2));
+    mainMenu.classList.toggle("main-menu--closed");
+    timeMenu.classList.toggle("time-menu--closed");
+    document.dispatchEvent(new CustomEvent('selectedTime', { detail: button.querySelector(".menu__subtext").textContent}))
+  });
+});
+
